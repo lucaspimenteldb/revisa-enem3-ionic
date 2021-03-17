4959e7616517c3918dbc1db15a3a0fe168fb6d0b
larciu <laerciodasilvapedrosa@gmail.com>
2021-03-17 16:57:36 -0300

    Redações entregues

--- src/views/redacoes/RedacoesEntregues.vue
+++ src/views/redacoes/RedacoesEntregues.vue
@@ -21,26 +21,25 @@
         <ion-item
             class="ion-margin-top rounded"
             v-for="atividade in atividades"
-            :key="atividade.tema"
-            @click="() => router.push('ver-correcao-redacao')"
+            :key="atividade.id"
+            @click="irAvaliacao(atividade)"
             lines="none"
         >
           <div class="ion-padding-vertical flex flex-1 ion-justify-content-between ion-align-items-center">
             <ion-label class="text-white text-lg font-bold">
               <p class="text-white font-regular">
-                Tema {{ atividade.tema }}
               </p>
 
-              {{ atividade.ttl }}
+              {{ atividade.titulo }}
             </ion-label>
 
             <div
                 class="flex ion-align-items-center ion-justify-content-center w-60 h-60 bg-white border-3 rounded-full"
                 :class="`border-${atividade.desempenho}`"
-                v-if="atividade.corrigida"
+                v-if="atividade.resultado !== null"
             >
               <ion-text class="font-bold text-lg" color="primary">
-                {{atividade.nota}}
+                {{atividade.resultado}}
               </ion-text>
             </div>
           </div>
@@ -51,11 +50,12 @@
               color="warning"
               size="large"
               class="mr-8"
-              v-if="!atividade.corrigida"
+              v-if="atividade.resultado === null"
           />
 
         </ion-item>
       </ion-list>
+      <Loading :isOpen="loading"></Loading>
     </ion-content>
   </ion-page>
 </template>
@@ -64,12 +64,18 @@
 import {IonPage, IonContent, IonItem, IonLabel, IonList, IonIcon, IonSelect, IonSelectOption, IonText} from '@ionic/vue';
 import {notifications, arrowForwardCircleOutline, appsOutline, checkmarkDoneOutline, alarmOutline} from 'ionicons/icons';
 import { useRouter } from 'vue-router'
+import Loading from "../../components/auxiliares/Loading";
+import {ref} from 'vue';
+import storage from "../../storage/StorageKey";
+import api from "../../api/basicUrl";
 
 export default {
   name: 'RedaçõesEntregues',
-  components: {IonPage, IonContent, IonItem, IonLabel, IonList, IonIcon, IonSelect, IonSelectOption, IonText},
+  components: {Loading, IonPage, IonContent, IonItem, IonLabel, IonList, IonIcon, IonSelect, IonSelectOption, IonText},
 
   setup () {
+    const loading = ref(false);
+    const user = ref({});
     return {
       router: useRouter(),
       notifications,
@@ -77,50 +83,76 @@ export default {
       appsOutline,
       checkmarkDoneOutline,
       alarmOutline,
-
+      loading,
+      user,
       atividades: [
-        {
-          ttl: 'XYZ mais eu',
-          tema: '0',
-          rota: 'correcao-simulados-entregues',
-          nota: 880,
-          desempenho: 'excelente',
-          corrigida: true
-        },
-        {
-          ttl: 'Pei pei pei',
-          tema: '1',
-          rota: 'correcao-atividades-entregues',
-          nota: 800,
-          desempenho: 'excelente',
-          corrigida: true
-        },
-        {
-          ttl: 'Xablau brau',
-          tema: '2',
-          rota: 'correcao-simulados-entregues',
-          nota: 920,
-          desempenho: 'excelente',
-          corrigida: false
-        },
-        {
-          ttl: 'Xablau brausd',
-          tema: '3',
-          rota: 'correcao-simulados-entregues',
-          nota: 100,
-          desempenho: 'muito-ruim',
-          corrigida: true
-        },
-        {
-          ttl: 'Xablau brausasdd',
-          tema: '34',
-          rota: 'correcao-simulados-entregues',
-          nota: 100,
-          desempenho: 'muito-ruim',
-          corrigida: true
-        },
+        // {
+        //   ttl: 'XYZ mais eu',
+        //   tema: '0',
+        //   rota: 'correcao-simulados-entregues',
+        //   nota: 880,
+        //   desempenho: 'excelente',
+        //   corrigida: true
+        // },
+        // {
+        //   ttl: 'Pei pei pei',
+        //   tema: '1',
+        //   rota: 'correcao-atividades-entregues',
+        //   nota: 800,
+        //   desempenho: 'excelente',
+        //   corrigida: true
+        // },
+        // {
+        //   ttl: 'Xablau brau',
+        //   tema: '2',
+        //   rota: 'correcao-simulados-entregues',
+        //   nota: 920,
+        //   desempenho: 'excelente',
+        //   corrigida: false
+        // },
+        // {
+        //   ttl: 'Xablau brausd',
+        //   tema: '3',
+        //   rota: 'correcao-simulados-entregues',
+        //   nota: 100,
+        //   desempenho: 'muito-ruim',
+        //   corrigida: true
+        // },
+        // {
+        //   ttl: 'Xablau brausasdd',
+        //   tema: '34',
+        //   rota: 'correcao-simulados-entregues',
+        //   nota: 100,
+        //   desempenho: 'muito-ruim',
+        //   corrigida: true
+        // },
       ]
     }
+  },
+
+  methods: {
+    irAvaliacao (redacao) {
+      if (redacao.resultado) {
+        this.router.push({path:'ver-correcao-redacao' , query: {redacao: JSON.stringify(redacao)}});
+      }
+    }
+  },
+
+  async ionViewWillEnter() {
+    try{
+      this.loading = true;
+      let usuario = await storage.get('user');
+      usuario = JSON.parse(usuario.value);
+      this.user = usuario;
+
+      let dados = await api.get('/redacoes-entregues/'+this.user.id);
+      this.atividades = dados.data.redacoes;
+
+    }catch (e) {
+      console.log(e);
+    }
+
+    this.loading = false;
   }
 }
 </script>
--- src/views/redacoes/VerCorrecaoRedacao.vue
+++ src/views/redacoes/VerCorrecaoRedacao.vue
@@ -4,10 +4,10 @@
     <ion-content >
       <ion-list>
         <ion-label class="ion-padding font-bold text-lg">
-          Correção da Redação - Tema 0
+          Correção da Redação - {{redacao.titulo}}
 
           <p class="ml-16">
-            Tema da redação aqui
+            {{redacao.titulo}}
           </p>
         </ion-label>
 
@@ -17,35 +17,35 @@
               Avaliação do corretor
             </p>
 
-            <p>
-              Olá! Lorem ipsum dolor sit amet, consectetur adipisicing elit. At ipsa iure maiores perferendis vel! Aperiam consequatur deserunt eligendi eum excepturi laborum nisi nostrum quidem. Asperiores aspernatur facilis molestiae nobis tenetur.
+            <p style="white-space: pre-line; text-align: left">
+              {{redacao.avaliacao}}
             </p>
 
             <h4>
-              Nota: 1000/1000
+              Nota: {{redacao.resultado}}/1000
             </h4>
           </ion-text>
         </div>
 
         <ion-item
             class="ion-margin-top flex ion-align-items-center ion-justify-content-between lista__professores"
-            v-for="opcao in opcoes"
-            :key="opcao.ttl+ 'competencia'"
+            v-for="(opcao, index) in opcoes"
+            :key="index"
             lines="none"
         >
           <div class="ion-margin-vertical mr-8 flex ion-align-items-center ion-justify-content-center bg-primary rounded-full h-40 w-40">
             <ion-text class="text-white text-sm font-bold">
-              {{ opcao.nota}}
+              {{ opcao.resultado}}
             </ion-text>
           </div>
 
           <ion-text class="ion-margin-vertical mr-8 text-xs">
-            {{ opcao.ttl }}
+            {{ opcao.descricao }}
           </ion-text>
 
           <div class="ml-auto">
             <ion-text class="">
-              {{ opcao.competencia }}
+              {{ 'C'+(index + 1) }}
             </ion-text>
           </div>
         </ion-item>
@@ -57,45 +57,56 @@
 <script>
 import {IonPage, IonContent, IonItem, IonLabel, IonList, IonText} from '@ionic/vue';
 import {notifications, arrowForwardCircleOutline} from 'ionicons/icons';
-import { useRouter } from 'vue-router'
+import { useRouter, useRoute } from 'vue-router'
+import {ref} from 'vue';
+
 export default {
   name: 'VerCorrecaoRedacao',
   components: {IonPage, IonContent, IonItem, IonLabel, IonList, IonText},
 
   setup () {
+    const redacao = ref({});
     return {
       notifications,
       arrowForwardCircleOutline,
       router: useRouter(),
+      route: useRoute(),
+      redacao,
 
       opcoes: [
-        {
-          ttl: 'Compreender o tema e não fugir do que é proposto',
-          competencia: 'C1',
-          nota: 120,
-        },
-        {
-          ttl: 'Selecionar, relacionar, contextualizar e interpretar informações, fatos, opiniões e argumentos em defesa de um ponto de vista',
-          competencia: 'C2',
-          nota: 200,
-        },
-        {
-          ttl: 'Conhecimento dos mecanismos linguísticos necessários para construção da argumentação',
-          competencia: 'C3',
-          nota: 200,
-        },
-        {
-          ttl: 'Respeito aos direitos humanos',
-          competencia: 'C4',
-          nota: 160,
-        },
-        {
-          ttl: 'Domínio da escrita da língua portuguesa',
-          competencia: 'C5',
-          nota: 200,
-        },
+        // {
+        //   ttl: 'Compreender o tema e não fugir do que é proposto',
+        //   competencia: 'C1',
+        //   nota: 120,
+        // },
+        // {
+        //   ttl: 'Selecionar, relacionar, contextualizar e interpretar informações, fatos, opiniões e argumentos em defesa de um ponto de vista',
+        //   competencia: 'C2',
+        //   nota: 200,
+        // },
+        // {
+        //   ttl: 'Conhecimento dos mecanismos linguísticos necessários para construção da argumentação',
+        //   competencia: 'C3',
+        //   nota: 200,
+        // },
+        // {
+        //   ttl: 'Respeito aos direitos humanos',
+        //   competencia: 'C4',
+        //   nota: 160,
+        // },
+        // {
+        //   ttl: 'Domínio da escrita da língua portuguesa',
+        //   competencia: 'C5',
+        //   nota: 200,
+        // },
       ]
     }
+  },
+
+  ionViewWillEnter () {
+    this.redacao = JSON.parse(this.route.query.redacao);
+    this.opcoes = this.redacao.competencias;
+    console.log(this.redacao);
   }
 }
 </script>
