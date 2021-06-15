afb64dbf98df2653fa12fd49a8cbe8bcfec8a539
Mac <mac@MacBook-Pro-de-Mac-2.local>
2021-06-15 09:45:52 -0300

    inicio pegando

--- src/mixins/VersionCode.js
+++ src/mixins/VersionCode.js
@@ -5,18 +5,7 @@ const objeto  = {
         async getVersionCode () {
             if (window.cordova) {
                 try{
-                    let version = await window.cordova.getAppVersion.getVersionNumber();
-                    let idPackage =  await window.cordova.getAppVersion.getPackageName();
-                    let dados = await api.get('/get-version');
-                    console.log(idPackage);
-                    let versionServer = dados.data.version;
                     this.loading = false;
-
-                    if (version != versionServer) {
-                        this.text.message = 'Possui uma atualização disponível na playstore';
-                        this.buttons = [{text: 'Atualizar', handler: () =>window.cordova.plugins.market.open(idPackage)}];
-                        this.dialog = true;
-                    }
 
                 }catch (e) {
                     console.log(e);
